08de49c9af3f5ee96574cdefc783c935d94853cb
Adam Schonely <aschonely@gmail.com>
2019-07-20 20:04:37 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,9 @@
 def unsafe?(speed)
-  if speed < 40 || speed > 60
+  if speed > 40
     puts 'This is an unsafe speed!'
-  elsif speed > 40 || speed > 60
+  elsif speed < 60
+    puts 'This is a unsafe speed!'
+  else 
     puts 'This is a safe speed.'
   end
 end
